c1232c788c7228dfe1dc782c8a14a0989f1126da
Masatoshi Urakawa <bokunolog@gmail.com>
2021-10-23 02:51:44 +0900

    ベース作成

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+$(function(){
+/* ==================================================
+
+	スムーススクロール
+
+=================================================== */
+
+	$('a[href^="#"]').on('click', function(){
+    	var speed    = 400;
+	    var href     = $(this).attr("href");
+ 		var target   = $(href == "#" || href == "" ? 'html' : href);
+	    var position = target.offset().top;
+
+		$('body, html').animate({ scrollTop: position }, speed, 'swing');
+		return false;
+	});
+
+/* ==================================================
+
+	ヘッダー固定
+
+=================================================== */
+
+	var beforePos     = 0;
+	var $headerFixed  = $(".js-headerFixed");
+	var $headerTarget = $(".js-headerTarget");
+
+	function ScrollAnime() {
+    	var elemTop = $headerTarget.offset().top;
+		var scroll  = $(window).scrollTop();
+
+	    if(scroll == beforePos) {
+	
+	    } else if(elemTop > scroll || 0 > scroll - beforePos) {
+			$headerFixed.removeClass("is-upMove");
+			$headerFixed.addClass("is-downMove");
+	    } else {
+        	$headerFixed.removeClass("is-downMove");
+			$headerFixed.addClass("is-upMove");
+    	}
+
+	    beforePos = scroll;
+	}
+
+	$(window).scroll(function () {
+		ScrollAnime();
+	});
+
+	$(window).on('load', function () {
+		ScrollAnime();
+	});
+});
